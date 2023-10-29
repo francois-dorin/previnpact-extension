c402f023dd2bcddefd41573638d9aa0c26bee8da
François DORIN <github@custom>
2023-10-29 13:23:46 +0100

    Refactoring + possibilité de choisir une police sans sérif

--- dist/scripts/popup.js
+++ dist/scripts/popup.js
@@ -1,18 +1,57 @@
 const darkModeCheckBox = document.querySelector('input[name="dark-mode"]');
+const policeSansSerifCheckBox = document.querySelector('input[name="police-sans-serif"]');
 const setDarkMode = () => {
     const value = darkModeCheckBox.checked;
-    (chrome ?? browser).storage.local.set({ darkMode: value });
+    extensionRuntime.setSettings({ darkMode: value });
+};
+const setPoliceSansSerif = () => {
+    const value = policeSansSerifCheckBox.checked;
+    extensionRuntime.setSettings({ policeSansSerif: value });
 };
 darkModeCheckBox.addEventListener('change', setDarkMode);
+policeSansSerifCheckBox.addEventListener('change', setPoliceSansSerif);
 const restoreOptions = () => {
-    (chrome ?? browser).storage.local.get(['darkMode']).then((values) => {
+    extensionRuntime.getSettings(['darkMode', 'policeSansSerif']).then((values) => {
         if (values.darkMode === undefined) {
             darkModeCheckBox.checked = true;
-            (chrome ?? browser).storage.local.set({ darkMode: true });
+            extensionRuntime.setSettings({ darkMode: true });
         }
         else {
             darkModeCheckBox.checked = values.darkMode;
         }
+        if (values.policeSansSerif === undefined) {
+            policeSansSerifCheckBox.checked = false;
+            extensionRuntime.setSettings({ policeSansSerif: false });
+        }
+        else {
+            policeSansSerifCheckBox.checked = values.policeSansSerif;
+        }
     });
 };
 document.addEventListener("DOMContentLoaded", restoreOptions);
+class ExtensionRuntime {
+    browser;
+    constructor() {
+        this.browser = this.getBrowser();
+    }
+    setSettings(values) {
+        this.browser.storage.local.set(values);
+    }
+    getSettings(keys) {
+        const callback = (resolve, values) => {
+            console.log('settings', values);
+            resolve(values);
+        };
+        const promise = new Promise((resolve) => {
+            this.browser.storage.local.get(keys).then((values) => callback(resolve, values));
+        });
+        return promise;
+    }
+    onChanged(callback) {
+        this.browser.storage.local.onChanged.addListener(callback);
+    }
+    getBrowser() {
+        return (chrome ?? browser);
+    }
+}
+const extensionRuntime = new ExtensionRuntime();
